bc1282399be89393dadb40abca3f704ceca125cf
kaiserino1002 <kaiserino1002@gmail.com>
2025-08-06 15:22:01 +0900

    add auth setup

---
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class GoogleAuthController extends Controller
+{
+      public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    // Googleからのコールバック
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        // ユーザー登録 or ログイン
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                // 必要なら追加カラムもここに
+            ]
+        );
+
+        Auth::login($user, true);
+
+        // SPAトップへリダイレクト
+        return redirect(env('FRONTEND_URL', 'http://localhost:8000'));
+    }
+}
